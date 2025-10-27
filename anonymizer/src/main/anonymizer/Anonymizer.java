fbc4346cfab9c6ab9e4fc32786a1a1fa796a98f9
Radovan Monček <radovan.moncek1@gmail.com>
2025-10-27 16:33:38 +0100

    refactor: feat: format code, and prepare for further implementation

--- anonymizer/src/main/anonymizer/Anonymizer.java
+++ anonymizer/src/main/anonymizer/Anonymizer.java
@@ -17,49 +17,36 @@ import java.util.concurrent.CountDownLatch;
 import java.util.Arrays;
 import java.io.*;
 import java.nio.*;
+import java.util.concurrent.*;
 
 import org.capnproto.*;
 
 /**
-* References:
-* https://kafka.apache.org/41/documentation/streams/tutorial
-* https://www.baeldung.com/apache-kafka
-*/
+ * References:
+ * https://kafka.apache.org/41/documentation/streams/tutorial
+ * https://www.baeldung.com/apache-kafka
+ */
 public class Anonymizer {
 	private static final Logger log = Logger.getLogger(Anonymizer.class.getName());
 
 	public static void main(String[] args) {
 		log.setLevel(Level.ALL);
-		
-//final var rootHandler = 
 		log.getParent().getHandlers()[0].setLevel(Level.ALL);
 		log.info("Anonymizer running");
-		
+
 		final var props = new Properties();
 		final var streamsBuilder = new StreamsBuilder();
-	
-		//props.put(StreamsConfig.APPLICATION_ID_CONFIG, "http_anonymization");	
+
 		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
 		props.put(ConsumerConfig.GROUP_ID_CONFIG, "data-engineering-task-reader");
 		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
 		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
-		//final var stream = streamsBuilder.stream("http_log");
-		//final var topology = streamsBuilder.build();
-
-		//log.info(topology.describe().toString());
-		//stream.print(Printed.toSysOut());
-
-		//final var streams = new KafkaStreams(topology, props);
-
-		//streams.start();
-
-		//while(true){}
 
 		try(final var consumer = new KafkaConsumer<Long, byte[]>(props)){
 			consumer.subscribe(Arrays.asList("http_log"));
 
-			final var queue = new ConcurrentLinkedQueue<ConsumerRecord<Long, byte[]>>();
+			final var queue = new LinkedBlockingQueue<ConsumerRecord<Long, byte[]>>();
 			final var bufferingThread = new Thread(() -> {
 				while(true) {
 					for (final var record : consumer.poll(Duration.ofMillis(100))) {
@@ -69,37 +56,56 @@ public class Anonymizer {
 			});
 			final var processingThread = new Thread(() -> {
 				while (true) {
-				//consumer.poll(Duration.ofMinutes(1)).records("http_log").forEach(record -> {
-				try {
-					//var bytes = "";
+					try {
+						final var current = queue.poll(10, TimeUnit.MILLISECONDS);
 
-					//for(var i = 0; i < record.value().length; ++i) {
-					//log.info(record.value()[i]);
-						//bytes += record.value()[i];
-					//}
+						if (current == null)
+							continue;
 
-					log.info(bytes);
-					
-					final var message = org.capnproto.Serialize.read(new ArrayInputStream(ByteBuffer.wrap(/*record.value()*/queue.poll())));
-					
-					log.info(message.toString());
+						final var message = org.capnproto.Serialize.read(new ArrayInputStream(ByteBuffer.wrap(current.value())));
+						final var httpLogRecord = message.getRoot(HttpLogRecordOuter.HttpLogRecord.factory);
 
-					final var httpLogRecord = message.getRoot(HttpLogRecordOuter.HttpLogRecord.factory);
-
-					log.info(httpLogRecord.getRemoteAddr().toString());
+						log.info(httpLogRecord.getRemoteAddr().toString());
+					}
+					catch (final Exception exception) {
+						log.throwing(Anonymizer.class.getName(), "main", exception);
+					}
 				}
-				catch (final Exception exception) {
-					log.throwing(Anonymizer.class.getName(), "main", exception);
-				//log.info(Arrays.asList(record.value()).stream().map(value -> "" + value).reduce("", (current, next) -> current + next));
-				//log.info(record.value().length() + "");
-				//log.info(new String(record.value()));
-				//System.out.println(record.value());
-				}
-			}
 			});
+
+			bufferingThread.start();
+			processingThread.start();
 		}
 		catch (final Exception exception) {
 			log.throwing(Anonymizer.class.getName(), "main", exception);
+		}
+	}
+
+	private interface DAO<T> {
+		T save(T object);
+		T query(int id);
+	}
+
+	private class CacheDAO implements DAO<ConsumerRecord<Long, byte[]> {
+		private final LinkedBlockingQueue<ConsumerRecord<Long, byte[]>> fakeCache = new LinkedBLockingQueue<>();
+		
+		public ConsumerRecord<Long, byte[]> save(ConsumerRecord<Long, byte[]> object) {
+			fakeCache.add(object);
+
+			return object;
+		}
+
+		public ConsumerRecord<Long, byte[]> query(int ignored) {
+			return fakeCache.poll(10, TimeUnit.MILLISECONDS);
+		}
+	}
+
+	private class ClickHouseDAO implements DAO<Object> {
+
+		public HttpLogRecord save(HttpLogRecord object) {
+			
+
+			return object;
 		}
 	}
 }
